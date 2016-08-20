342a8ca161bcd34f12d48281cc9a9089fb27e494
sam-bailey <samcbailey90@gmail.com>
2016-08-20 20:05:36 +0100

    Basic initialisation of the data frames
    A basic data frame structure was created, and a simple function to look
    at messages over time was made to test this.

--- tests/whatsapp_chat_test.py
+++ tests/whatsapp_chat_test.py
@@ -1,4 +1,9 @@
 from whatsanalysis.whatsapp_chat import WhatsAppChat
+import matplotlib.pyplot as plt
 
 file_name = '../whatsapp-conversations/Fingers_chat.convo'
 test_chat = WhatsAppChat(file_name)
+
+test_chat.find_people()
+test_chat.plot_messages_over_time(person=['Feebs'], bins=50, alpha=0.5)
+plt.show()
